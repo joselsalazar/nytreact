2e3772bf15f4bedad2e36f44fb85c9daf8d33cd4
Jose Salazar <jose.salazar.home@gmail.com>
2017-08-29 20:09:44 -0700

    Routes Files Added

--- server.js
+++ server.js
@@ -33,6 +33,11 @@ db.once("open", function() {
   console.log("Mongoose connection successful.");
 });
 
+// Routes
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+}); 
+
 // Listener
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);

416af76531c34961610237dd7a72953440744c80
Jose Salazar <jose.salazar.home@gmail.com>
2017-08-30 19:54:26 -0700

    FInal Changes 08-30-17

---
+++ app/components/Main.js
@@ -0,0 +1,15 @@
+var React = require("react");
+
+var Main = React.createClass({
+	render: function() {
+		return (
+			<div className="panel panel-default">
+			  <div className="panel-heading">
+			    <h3 className="panel-title text-center">Search History</h3>
+			  </div>
+			  <div className="panel-body text-center">
+			  </div>
+			</div>
+		);
+	}
+});
